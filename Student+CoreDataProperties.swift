1a870857ed82c190b70088003774fede46045930
linwei365 <linwei365@gmail.com>
2016-02-10 21:47:35 -0800

    removed viewController and added new tableViewController and made new class

---
+++ Student+CoreDataProperties.swift
@@ -0,0 +1,19 @@
+//
+//  Student+CoreDataProperties.swift
+//  StudentCoreDataDay2
+//
+//  Created by Lin Wei on 2/10/16.
+//  Copyright © 2016 Lin Wei. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension Student {
+
+    @NSManaged var name: String?
+
+}
---
+++ Student.swift
@@ -0,0 +1,17 @@
+//
+//  Student.swift
+//  StudentCoreDataDay2
+//
+//  Created by Lin Wei on 2/10/16.
+//  Copyright © 2016 Lin Wei. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Student: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
